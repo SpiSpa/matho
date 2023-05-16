8339e6aba4c25f3067ec655d14aa2ef2e6326d34
sarab <sarab@SHEEP>
2023-05-16 13:06:13 -0500

    Changed test data from user inputed to a hard coded list.  Updated comments.  Reorganized outupt display

--- MATHO.py
+++ MATHO.py
@@ -4,13 +4,13 @@ import random
 
 xCoord = 0
 yCoord = 0
-master_list = []
+master_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
 
 print(master_list)
 
-for entry in range(25):
-    newInput = int(input("Enter a new number"))
-    master_list.append(newInput)
+#for entry in range(25):     
+ #   newInput = int(input("Enter a new number"))
+  #  master_list.append(newInput)
 
 print(master_list)
 
@@ -22,25 +22,25 @@ count = 0
 
 for count in range(25):
     print("count:", count)
-    xRandom = random.randint(0,4)  #first hash
-    yRandom = random.randint(0,4)    #first hash
-    print("First Hash", xRandom, yRandom)
+    xRandom = random.randint(0,4)  #x value of a randomized location
+    yRandom = random.randint(0,4)    #y value of randimized location
+    print("First Random", xRandom, yRandom)
 
     if randomizedCard[xRandom][yRandom] == "":
         randomizedCard[xRandom][yRandom] = master_list[count]   #check that the spot is empty
         
     else:
-        print("Collision!")
-        xRandom = random.randint(0,4)  #second hash
+        print("Location Already Fo;;ed")
+        xRandom = random.randint(0,4)  # if the randomized location is already taken, try a new randomized location
         yRandom = random.randint(0,4)    #second hash
-        print("Second hash", xRandom, yRandom)
+        print("Second random", xRandom, yRandom)
 
         if randomizedCard[xRandom][yRandom] == "":
             randomizedCard[xRandom][yRandom] = master_list[count]
         else:
-            print("Collision")
+            print("Location already filled")
             
-            while randomizedCard[xRandom][yRandom] != "":
+            while randomizedCard[xRandom][yRandom] != "":  # if the second randomized location is also taken, find another spot that is open
                 xcount = 0
                 yRandom = (yRandom + 7) % 5
                 print("y-random", yRandom)
@@ -51,5 +51,5 @@ for count in range(25):
                     
             randomizedCard[xRandom][yRandom] = master_list[count]
             
-print("test")            
-print(randomizedCard)
+for i in range(5):
+    print(randomizedCard[i][0], randomizedCard[i][1],randomizedCard[i][2],randomizedCard[i][3],randomizedCard[i][4])
